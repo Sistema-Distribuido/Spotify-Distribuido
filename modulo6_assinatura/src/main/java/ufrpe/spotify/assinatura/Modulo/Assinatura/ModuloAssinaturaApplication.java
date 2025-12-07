5176bf88e838c62d534e486bf0ff6d4777c3b355
Pedro Henrique <pedro.apolinario@ufrpe.br>
2025-12-07 18:50:45 -0300

    Estrutura springboot do módulo de assinatura

---
+++ modulo6_assinatura/src/main/java/ufrpe/spotify/assinatura/Modulo/Assinatura/ModuloAssinaturaApplication.java
@@ -0,0 +1,13 @@
+package ufrpe.spotify.assinatura.Modulo.Assinatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloAssinaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloAssinaturaApplication.class, args);
+	}
+
+}
---
+++ modulo6_assinatura/src/test/java/ufrpe/spotify/assinatura/Modulo/Assinatura/ModuloAssinaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrpe.spotify.assinatura.Modulo.Assinatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloAssinaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
