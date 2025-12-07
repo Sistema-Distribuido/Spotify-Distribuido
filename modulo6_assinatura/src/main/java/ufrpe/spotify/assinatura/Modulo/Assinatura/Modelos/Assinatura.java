60cbf2dd9775860f264ba326e5925e4cf5e25792
Pedro Henrique <pedro.apolinario@ufrpe.br>
2025-12-07 18:56:56 -0300

    Classe básica de assinatura

---
+++ modulo6_assinatura/src/main/java/ufrpe/spotify/assinatura/Modulo/Assinatura/Modelos/Assinatura.java
@@ -0,0 +1,35 @@
+package ufrpe.spotify.assinatura.Modulo.Assinatura.Modelos;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="Assinatura")
+@Data
+public class Assinatura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // Apenas armazenamos o ID do usuário (chave estrangeira lógica)
+    @Column(name = "user_id", unique = true, nullable = false)
+    private UUID userId;
+
+    @Enumerated(EnumType.STRING)
+    private PlanType plano;
+
+    @Enumerated(EnumType.STRING)
+    private SubscriptionStatus status;
+
+    private LocalDateTime fimAssinatura;
+
+    // Enums auxiliares
+    public enum PlanType { FREE, PREMIUM, ADMIN }
+    public enum SubscriptionStatus { ATIVO, CANCELADO, PENDENTE }
+}
+
